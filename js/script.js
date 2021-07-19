38bac69bd98af7c074c5b4e9d60f56312138473a
Matteo <grecomatteo86@gmail.com>
2021-07-19 15:15:09 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 var nome = prompt('Quale è il tuo nome?');
 var cognome = prompt('Quale è il tuo cognome?');
 var colore = prompt('Quale è il tuo colore preferito?');
-
 var messaggio = nome + cognome + colore + '27';
-
 document.getElementById('risultato').innerHTML = messaggio;
